18649e79408fb1ba94ff5e6370d6e8d7905c01b6
zabiullah-shokuri <zabitawana4@gmail.com>
2024-10-24 09:36:58 +0330

    Add header

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import MenuItem from "./MenuItem";
+import { AiFillHome } from 'react-icons/ai';
+import { BsFillInfoCircleFill } from 'react-icons/bs'
+
+
+
+function Header() {
+
+
+  return (
+
+    <div className="flex justify-between items-center p-3 max-w-6xl mx-auto">
+
+      <div className="flex gap-4">
+        <MenuItem title="home" address="/" Icon={ AiFillHome} />
+        <MenuItem title="about" address="/about" Icon={ BsFillInfoCircleFill} />
+      </div>
+
+
+    <div className="flex gap-1 items-center">
+        <span className="text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg">ZabiApp</span>
+        <span className="text-xl hidden sm:inline">Clone</span>
+    </div>
+      
+    </div>
+
+  )
+}
+
+export default Header;
+
+
+
